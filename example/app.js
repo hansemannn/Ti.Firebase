32e534e86c35fd25581b9abc8772f95cbd66b2bf
Hans Knöchel <hansemannn@users.noreply.github.com>
2017-09-19 00:31:33 +0200

    Update app.js

--- example/app.js
+++ example/app.js
@@ -6,15 +6,15 @@ if (Firebase.initFirebaseApp()) {
 	var Auth = Firebase.createAuthentication();
 	Auth.signInAnonymously({
 		onComplete: function(_event) {
-      if (_event) {
-        console.log(_event);
-      }
+      			if (_event) {
+        			console.log(_event);
+      			}	
  		}
 	}
 });
 
 Auth.addEventListener('onAuthStateChanged', function(e) {
-  if (e) {
-    console.log(e.user);
-  }
+	if (e) {
+    		console.log(e.user);
+  	}
 });
